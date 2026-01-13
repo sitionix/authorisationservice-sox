63f684f5218c068a1b2e43840c02992e4e119da3
vladvinskevitch <vvinskevitch@gmail.com>
2026-01-13 08:29:06 +0200

    [SITIONIX-58] - add more tests for session

--- infrastructure/postgresql/src/main/java/com/sitionix/athssox/postgresql/entity/session/SessionStatusEntity.java
+++ infrastructure/postgresql/src/main/java/com/sitionix/athssox/postgresql/entity/session/SessionStatusEntity.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "SESSION_STATUSES")
 @Getter
