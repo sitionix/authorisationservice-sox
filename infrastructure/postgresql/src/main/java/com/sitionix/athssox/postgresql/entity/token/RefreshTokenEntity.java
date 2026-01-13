afaba95a2b911fc8522c3f20c74ec8df203522d5
vladvinskevitch <vvinskevitch@gmail.com>
2026-01-13 08:33:15 +0200

    [SITIONIX-58] - add more tests for session

--- infrastructure/postgresql/src/main/java/com/sitionix/athssox/postgresql/entity/token/RefreshTokenEntity.java
+++ infrastructure/postgresql/src/main/java/com/sitionix/athssox/postgresql/entity/token/RefreshTokenEntity.java
@@ -16,7 +16,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
