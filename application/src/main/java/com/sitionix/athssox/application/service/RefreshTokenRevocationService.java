a799f6292afd99617d22d6b974ab3dcf6b52d125
vladvinskevitch <vvinskevitch@gmail.com>
2026-01-21 12:52:15 +0200

    [SITIONIX-64] - add more security

--- application/src/main/java/com/sitionix/athssox/application/service/RefreshTokenRevocationService.java
+++ application/src/main/java/com/sitionix/athssox/application/service/RefreshTokenRevocationService.java
@@ -1,32 +1,24 @@
 package com.sitionix.athssox.application.service;
 
 import com.sitionix.athssox.domain.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
 @Service
+@RequiredArgsConstructor
 public class RefreshTokenRevocationService {
 
     private final RefreshTokenRepository refreshTokenRepository;
-    private final TransactionTemplate transactionTemplate;
 
-    public RefreshTokenRevocationService(final RefreshTokenRepository refreshTokenRepository,
-                                         final PlatformTransactionManager transactionManager) {
-        this.refreshTokenRepository = refreshTokenRepository;
-        this.transactionTemplate = new TransactionTemplate(transactionManager);
-        this.transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-    }
-
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public boolean revokeIfActive(final Long tokenId, final Instant now, final String reason) {
         if (tokenId == null) {
             return false;
         }
-        final Boolean revoked = this.transactionTemplate.execute(
-                status -> this.refreshTokenRepository.revokeIfActive(tokenId, now, reason));
-        return Boolean.TRUE.equals(revoked);
+        return this.refreshTokenRepository.revokeIfActive(tokenId, now, reason);
     }
 }
