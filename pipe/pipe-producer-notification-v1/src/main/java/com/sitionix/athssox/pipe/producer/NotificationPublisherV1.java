cb5a03ef4db30b876a3f2576be8f387afb2ec7ef
vladvinskevitch <vvinskevitch@gmail.com>
2026-03-05 16:29:25 +0200

    [SITIONIX-95] - Implement direct outbox publisher contract

--- pipe/pipe-producer-notification-v1/src/main/java/com/sitionix/athssox/pipe/producer/NotificationPublisherV1.java
+++ pipe/pipe-producer-notification-v1/src/main/java/com/sitionix/athssox/pipe/producer/NotificationPublisherV1.java
@@ -5,7 +5,7 @@ import com.app_afesox.ntfssox.events.notifications.kafka.NotificationsV1Producer
 import com.sitionix.athssox.domain.model.outbox.payload.EmailVerifyPayload;
 import com.sitionix.athssox.pipe.producer.mapper.NotificationEventMapper;
 import com.sitionix.forge.outbox.core.model.Event;
-import com.sitionix.forge.outbox.core.port.ForgeTypedOutboxEventPublisher;
+import com.sitionix.forge.outbox.core.port.ForgeOutboxEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,19 +13,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class NotificationPublisherV1 extends ForgeTypedOutboxEventPublisher<EmailVerifyPayload> {
+public class NotificationPublisherV1 implements ForgeOutboxEventPublisher<EmailVerifyPayload> {
 
     private final NotificationsV1Producer producer;
 
     private final NotificationEventMapper mapper;
 
     @Override
-    protected Class<EmailVerifyPayload> payloadClass() {
+    public Class<EmailVerifyPayload> payloadClass() {
         return EmailVerifyPayload.class;
     }
 
     @Override
-    protected void publish(final Event<EmailVerifyPayload> event) {
+    public void publish(final Event<EmailVerifyPayload> event) {
         log.info("Publish notification event");
         final String key = event.getIdempotencyId().toString();
         final NotificationEnvelope envelope = this.mapper.asEnvelope(event);
